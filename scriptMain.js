4f798f11e6cd4c3bdbf62a7d353249aabfd2f4ae
deelnase <Makimova2003@gmail.com>
2022-12-20 01:55:06 +0400

    script

--- scriptMain.js
+++ scriptMain.js
@@ -1,12 +1,363 @@
 $(document).ready(function(){
-    var ch = true;
-      $("#slideDown").click(function(){
-        if(ch == true){
-        $("#panel").slideDown(1000);
-        ch = false;
-        }else{
-        $("#panel").slideUp(1000);
-        ch = true;
-        }
-      });
+  var ch = true;
+    $("#slideDown").click(function(){
+      if(ch == true){
+      $("#panel").slideDown(1000);
+      ch = false;
+      }else{
+      $("#panel").slideUp(1000);
+      ch = true;
+      }
+    });
+});
+
+
+$(document).ready(function(){
+var check = true;
+  $("#qz1kn").click(function(){
+  if(check == true){
+    $("#qz1img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#qz1img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+
+$(document).ready(function(){
+var check = true;
+  $("#qz2kn").click(function(){
+    if(check == true){
+      $("#qz2img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz2img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+
+$(document).ready(function(){
+var check = true;
+  $("#qz3kn").click(function(){
+    if(check == true){
+      $("#qz3img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz3img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+
+$(document).ready(function(){
+var check = true;
+  $("#qz4kn").click(function(){
+    if(check == true){
+      $("#qz4img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz4img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+
+$(document).ready(function(){
+var check = true;
+  $("#qz5kn").click(function(){
+    if(check == true){
+      $("#qz5img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz5img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+
+$(document).ready(function(){
+var check = true;
+  $("#qz6kn").click(function(){
+    if(check == true){
+      $("#qz6img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz6img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#qz7kn").click(function(){
+    if(check == true){
+      $("#qz7img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz7img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#qz8kn").click(function(){
+    if(check == true){
+      $("#qz8img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz8img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#qz9kn").click(function(){
+    if(check == true){
+      $("#qz9img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz9img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#qz10kn").click(function(){
+    if(check == true){
+      $("#qz10img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz10img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#qz11kn").click(function(){
+    if(check == true){
+      $("#qz11img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz11img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#qz12kn").click(function(){
+    if(check == true){
+      $("#qz12img").fadeTo("slow", 0.15);
+    check = false;
+    }else{
+    $("#qz12img").fadeTo("slow", 1);
+    check = true;
+    }
+    });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#zh1kn").click(function(){
+  if(check == true){
+    $("#zh1img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#zh1img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#zh2kn").click(function(){
+  if(check == true){
+    $("#zh2img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#zh2img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#zh3kn").click(function(){
+  if(check == true){
+    $("#zh3img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#zh3img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#zh4kn").click(function(){
+  if(check == true){
+    $("#zh4img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#zh4img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#zh5kn").click(function(){
+  if(check == true){
+    $("#zh5img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#zh5img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+
+$(document).ready(function(){
+var check = true;
+  $("#zh6kn").click(function(){
+  if(check == true){
+    $("#zh6img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#zh6img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#zh7kn").click(function(){
+  if(check == true){
+    $("#zh7img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#zh7img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#ba1kn").click(function(){
+  if(check == true){
+    $("#ba1img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#ba1img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#ba2kn").click(function(){
+  if(check == true){
+    $("#ba2img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#ba2img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#ba3kn").click(function(){
+  if(check == true){
+    $("#ba3img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#ba3img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#ba4kn").click(function(){
+  if(check == true){
+    $("#ba4img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#ba4img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#ba5kn").click(function(){
+  if(check == true){
+    $("#ba5img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#ba5img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#ba6kn").click(function(){
+  if(check == true){
+    $("#ba6img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#ba6img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+var check = true;
+  $("#ba7kn").click(function(){
+  if(check == true){
+    $("#ba7img").fadeTo("slow", 0.15);
+  check = false;
+  }else{
+  $("#ba7img").fadeTo("slow", 1);
+  check = true;
+  }
+  });
+});
+
+$(document).ready(function(){
+$("#ani").click(function(){	
+  $("#app").animate({height: 'toggle'});
+});
 });
